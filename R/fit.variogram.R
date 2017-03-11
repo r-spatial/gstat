r-spatial/gstat

commit 5a52992a30d52bccc6da03eb2a54aaf2aa3f1f41

fix border case in vgm_fill_na

R/fit.variogram.R
@@ -87,7 +87,9 @@ vgm_fill_na = function(model, obj) {
 	}
 	if (any(model$model %in% "Nug") && is.na(model[model$model == "Nug","psill"]))
     	model[model$model == "Nug", "psill"] = mean(head(obj$gamma, 3))
-	if (is.na(model[model$model != "Nug","psill"]))
-    	model[model$model != "Nug", "psill"] = mean(tail(obj$gamma, 5))
+	if (any(is.na(model[model$model != "Nug", "psill"]))) {
+		n = length(na.omit(model[model$model != "Nug",]$psill))
+    	model[model$model != "Nug", "psill"] = mean(tail(obj$gamma, 5)) / n
+	}
 	model
 }

R/zzz.R
@@ -5,7 +5,7 @@
 .onLoad <- function(lib, pkg) {
 	# remove the require() call for 2.0.0:
 	# require(lattice)
-	.Call(gstat_init, as.integer(1))
+	# .Call(gstat_init, as.integer(1))
 }
 
 ### pre-NAMESPACE VERSION:

src/data.c
@@ -406,6 +406,10 @@ void free_data(DATA *d) {
     if (d->point_ids)
         for (i = d->n_list - 1; i >= 0; i--)
             efree(d->point_ids[i]);
+
+	if (d->beta)
+		efree(d->beta);
+
 	efree(d);
 	return;
 }

src/msim.c
@@ -255,6 +255,9 @@ void free_simulations(void) {
 		efree(beta);
 		beta = NULL;
 	}
+	if (n_sim_locs != NULL)
+		free(n_sim_locs);
+	n_sim_locs = NULL;
 }
 
 void setup_beta(DATA **d, int n_vars, int n_sim) {

commit 5ec17c4d02a3fa0c8c068bfe285caa886e21addf

fix #21
this is a correction of a regression introduced in https://github.com/edzer/gstat/commit/5a52992a30d52bccc6da03eb2a54aaf2aa3f1f41

R/fit.variogram.R
@@ -87,8 +87,9 @@ vgm_fill_na = function(model, obj) {
 	}
 	if (any(model$model %in% "Nug") && is.na(model[model$model == "Nug","psill"]))
     	model[model$model == "Nug", "psill"] = mean(head(obj$gamma, 3))
-	if (any(is.na(model[model$model != "Nug", "psill"]))) {
-		n = length(na.omit(model[model$model != "Nug",]$psill))
+	na_sills = is.na(model[model$model != "Nug", "psill"])
+	if (any(na_sills)) {
+		n = length(model[model$model != "Nug",]$psill)
     	model[model$model != "Nug", "psill"] = mean(tail(obj$gamma, 5)) / n
 	}
 	model