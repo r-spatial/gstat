r-spatial/gstat

commit d9db9c334a1acdd29f9e2307b594d676e0546e95

CI test

R/variogramST.R
@@ -116,21 +116,29 @@ variogramST = function(formula, locations, data, ..., tlags = 0:15, cutoff,
 	if (progress)
 	  pb = txtProgressBar(style = 3, max = length(tlags))
 	if (cores == 1){
-	  future::plan(sequential)
+    for (dt in seq(along = tlags)) {
+      ret[[dt]] = StVgmLag(formula, data, tlags[dt], pseudo = pseudo, 
+                          boundaries = boundaries, ...)
+      ret[[dt]]$id = paste("lag", dt - 1, sep="")
+      if (progress)
+        setTxtProgressBar(pb, dt)
+    }
 	} else {
+    if(!requireNamespace("future", quietly = TRUE) & !requireNamespace("future.apply", quietly = TRUE))
+      stop("For parallelization, future and future.apply packages are required")
 	  future::plan('multiprocess', workers = cores)
+  	ret <- split(seq(along=tlags), seq(along=tlags))
+    ret <- future.apply::future_lapply(X = ret,
+                                      FUN = function(x){
+                                        xx <- StVgmLag(formula, data, tlags[x], pseudo = pseudo, 
+                                                        boundaries = boundaries, ...)
+                                        xx$id <- paste("lag", x - 1, sep="")
+                                        if (progress)
+                                          setTxtProgressBar(pb, x)
+                                        return(xx)
+                                      })
 	}
 	
-	ret <- split(seq(along=tlags), seq(along=tlags))
-	ret <- future.apply::future_lapply(X = ret,
-	                                   FUN = function(x){
-	                                     xx <- StVgmLag(formula, data, tlags[x], pseudo = pseudo, 
-	                                                    boundaries = boundaries, ...)
-	                                     xx$id <- paste("lag", x - 1, sep="")
-	                                     if (progress)
-	                                       setTxtProgressBar(pb, x)
-	                                     return(xx)
-	                                   })
 	if (progress)
 		close(pb)
 	# add time lag:
@@ -220,8 +228,9 @@ variogramST.STIDF <- function (formula, data, tlags, cutoff,
     if (cores == 1){
       tmpInd[,3] <- apply(tmpInd[,1:2,drop=FALSE], 1, function(x) spDists(data@sp[x[1]], data@sp[x[2]+x[1],]))
     } else {
+      if(!requireNamespace("future", quietly = TRUE) & !requireNamespace("future.apply", quietly = TRUE))
+        stop("For parallelization, future and future.apply packages are required")
       future::plan("multiprocess", workers = cores)
-      
       tmpInd[,3] <- future_apply(X = tmpInd[,1:2,drop=FALSE], MARGIN = 1, 
                                  FUN = function(x) spDists(data@sp[x[1]], data@sp[x[2]+x[1],]))
     }

DESCRIPTION
@@ -13,7 +13,7 @@ Depends: R (>= 2.10)
 Imports: utils, stats, graphics, methods, lattice, sp (>= 0.9-72), zoo,
         spacetime (>= 1.0-0), FNN
 Suggests: fields, maps, mapdata, maptools, rgdal (>= 0.5.2), rgeos, sf
-        (>= 0.7-2), stars (>= 0.3-0), xts, raster
+        (>= 0.7-2), stars (>= 0.3-0), xts, raster, future, future.apply
 License: GPL (>= 2.0)
 URL: https://github.com/r-spatial/gstat/
 Encoding: UTF-8

NAMESPACE
@@ -30,8 +30,6 @@ importFrom(zoo, zoo, is.regular)
 import(sp)
 import(spacetime)
 import(FNN)
-import(future)
-import(future.apply)
 
 export(as.vgm.variomodel,
 	cross.name,

commit 989a8b7e759138aa210a7bce170fae6281d74122

fix problem introduced by #62
fixes #65

R/variogramST.R
@@ -115,22 +115,23 @@ variogramST = function(formula, locations, data, ..., tlags = 0:15, cutoff,
 	t = twidth * tlags
 	if (progress)
 	  pb = txtProgressBar(style = 3, max = length(tlags))
-	if (cores == 1){
-    	for (dt in seq(along = tlags)) {
-      		ret[[dt]] = StVgmLag(formula, data, tlags[dt], pseudo = pseudo, 
-                          	boundaries = boundaries, ...)
-      		ret[[dt]]$id = paste("lag", dt - 1, sep="")
-      		if (progress)
-        		setTxtProgressBar(pb, dt)
-    	}
+	if (cores == 1) {
+		ret = vector("list", length(tlags))
+		for (dt in seq(along = tlags)) {
+	  		ret[[dt]] = StVgmLag(formula, data, tlags[dt], pseudo = pseudo, 
+						  	boundaries = boundaries, ...)
+	  		ret[[dt]]$id = paste("lag", dt - 1, sep="")
+	  		if (progress)
+				setTxtProgressBar(pb, dt)
+		}
 	} else {
-    	if (!requireNamespace("future", quietly = TRUE) || 
+		if (!requireNamespace("future", quietly = TRUE) || 
 				!requireNamespace("future.apply", quietly = TRUE))
-      		stop("For parallelization, future and future.apply packages are required")
+	  		stop("For parallelization, future and future.apply packages are required")
 
 	  	future::plan('multiprocess', workers = cores)
   		ret <- split(seq(along=tlags), seq(along=tlags))
-    	ret <- future.apply::future_lapply(X = ret,
+		ret <- future.apply::future_lapply(X = ret,
 				FUN = function(x){
 					xx <- StVgmLag(formula, data, tlags[x], pseudo = pseudo, 
 					boundaries = boundaries, ...)
@@ -149,7 +150,7 @@ variogramST = function(formula, locations, data, ..., tlags = 0:15, cutoff,
 	v$timelag = rep(t, sapply(ret, nrow))
 	if (is(t, "yearmon"))
 		class(v$timelag) = "yearmon"
-    
+
 	b = attr(ret[[min(length(tlags),2)]], "boundaries")
 	b = c(0, b[2]/1e6, b[-1])
 	# ix = findInterval(v$dist, b) will use all spacelags
@@ -165,7 +166,7 @@ variogramST = function(formula, locations, data, ..., tlags = 0:15, cutoff,
 
 	class(v) = c("StVariogram", "data.frame")
 	if(na.omit)
-    v <- na.omit(v)
+	v <- na.omit(v)
 
 	# setting attributes to allow krigeST to check units
 	attr(v$timelag, "units") <- attr(twidth,"units")

tests/stars.Rout.save
@@ -1,6 +1,6 @@
 
-R version 3.6.2 (2019-12-12) -- "Dark and Stormy Night"
-Copyright (C) 2019 The R Foundation for Statistical Computing
+R version 3.6.3 (2020-02-29) -- "Holding the Windsock"
+Copyright (C) 2020 The R Foundation for Statistical Computing
 Platform: x86_64-pc-linux-gnu (64-bit)
 
 R is free software and comes with ABSOLUTELY NO WARRANTY.
@@ -118,7 +118,9 @@ Type 'q()' to quit R.
 [1] "Attributes: < Component \"bbox\": Attributes: < Component \"dimnames\": Component 1: 2 string mismatches > >"  
 [2] "Attributes: < Component \"coords\": Attributes: < Component \"dimnames\": Component 2: 2 string mismatches > >"
 [3] "Attributes: < Component \"coords.nrs\": Numeric: lengths (2, 0) differ >"                                      
-[4] "Attributes: < Component \"proj4string\": Attributes: < Component \"projargs\": 1 string mismatch > >"          
+[4] "Attributes: < Component \"proj4string\": Attributes: < Names: 1 string mismatch > >"                           
+[5] "Attributes: < Component \"proj4string\": Attributes: < Length mismatch: comparison on first 2 components > >"  
+[6] "Attributes: < Component \"proj4string\": Attributes: < Component 2: 1 string mismatch > >"                     
 > 
 > # 2. using stars for grid:
 > 
@@ -142,18 +144,102 @@ x    1  78 178440    40 +proj=sterea +lat_0=52.15... FALSE   NULL [x]
 y    1 104 333760   -40 +proj=sterea +lat_0=52.15... FALSE   NULL [y]
 > st = st_as_stars(meuse.grid)
 > all.equal(st_dimensions(st0), st_dimensions(st))
-[1] "Component \"x\": Component \"refsys\": 1 string mismatch"
-[2] "Component \"x\": Component \"point\": 'is.NA' value mismatch: 1 in current 0 in target"
-[3] "Component \"y\": Component \"refsys\": 1 string mismatch"
-[4] "Component \"y\": Component \"point\": 'is.NA' value mismatch: 1 in current 0 in target"
+ [1] "Component \"x\": Component \"refsys\": Modes: list, character"                              
+ [2] "Component \"x\": Component \"refsys\": names for target but not for current"                
+ [3] "Component \"x\": Component \"refsys\": Attributes: < Modes: list, NULL >"                   
+ [4] "Component \"x\": Component \"refsys\": Attributes: < names for target but not for current >"
+ [5] "Component \"x\": Component \"refsys\": Attributes: < current is not list-like >"            
+ [6] "Component \"x\": Component \"refsys\": Length mismatch: comparison on first 1 components"   
+ [7] "Component \"x\": Component \"refsys\": Component 1: 1 string mismatch"                      
+ [8] "Component \"x\": Component \"point\": 'is.NA' value mismatch: 1 in current 0 in target"     
+ [9] "Component \"y\": Component \"refsys\": Modes: list, character"                              
+[10] "Component \"y\": Component \"refsys\": names for target but not for current"                
+[11] "Component \"y\": Component \"refsys\": Attributes: < Modes: list, NULL >"                   
+[12] "Component \"y\": Component \"refsys\": Attributes: < names for target but not for current >"
+[13] "Component \"y\": Component \"refsys\": Attributes: < current is not list-like >"            
+[14] "Component \"y\": Component \"refsys\": Length mismatch: comparison on first 1 components"   
+[15] "Component \"y\": Component \"refsys\": Component 1: 1 string mismatch"                      
+[16] "Component \"y\": Component \"point\": 'is.NA' value mismatch: 1 in current 0 in target"     
 > st_crs(st0)
 Coordinate Reference System:
-  No EPSG code
-  proj4string: "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.4171,50.3319,465.5524,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs"
+  User input: unknown 
+  wkt:
+PROJCRS["unknown",
+    BASEGEOGCRS["unknown",
+        DATUM["Unknown_based_on_Bessel_1841_ellipsoid",
+            ELLIPSOID["Bessel 1841",6377397.155,299.1528128,
+                LENGTHUNIT["metre",1,
+                    ID["EPSG",9001]]]],
+        PRIMEM["Greenwich",0,
+            ANGLEUNIT["degree",0.0174532925199433,
+                ID["EPSG",9122]]]],
+    CONVERSION["Oblique Stereographic",
+        METHOD["Oblique Stereographic",
+            ID["EPSG",9809]],
+        PARAMETER["Latitude of natural origin",52.1561605555556,
+            ANGLEUNIT["degree",0.0174532925199433],
+            ID["EPSG",8801]],
+        PARAMETER["Longitude of natural origin",5.38763888888889,
+            ANGLEUNIT["degree",0.0174532925199433],
+            ID["EPSG",8802]],
+        PARAMETER["Scale factor at natural origin",0.9999079,
+            SCALEUNIT["unity",1],
+            ID["EPSG",8805]],
+        PARAMETER["False easting",155000,
+            LENGTHUNIT["metre",1],
+            ID["EPSG",8806]],
+        PARAMETER["False northing",463000,
+            LENGTHUNIT["metre",1],
+            ID["EPSG",8807]]],
+    CS[Cartesian,2],
+        AXIS["easting",east,
+            ORDER[1],
+            LENGTHUNIT["metre",1,
+                ID["EPSG",9001]]],
+        AXIS["northing",north,
+            ORDER[2],
+            LENGTHUNIT["metre",1,
+                ID["EPSG",9001]]]]
 > st_crs(st)
 Coordinate Reference System:
-  No EPSG code
-  proj4string: "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.2369,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs"
+  User input: +proj=sterea +lat_0=52.1561605555556 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs 
+  wkt:
+PROJCRS["unknown",
+    BASEGEOGCRS["unknown",
+        DATUM["Unknown based on Bessel 1841 ellipsoid",
+            ELLIPSOID["Bessel 1841",6377397.155,299.1528128,
+                LENGTHUNIT["metre",1,
+                    ID["EPSG",9001]]]],
+        PRIMEM["Greenwich",0,
+            ANGLEUNIT["degree",0.0174532925199433],
+            ID["EPSG",8901]]],
+    CONVERSION["unknown",
+        METHOD["Oblique Stereographic",
+            ID["EPSG",9809]],
+        PARAMETER["Latitude of natural origin",52.1561605555556,
+            ANGLEUNIT["degree",0.0174532925199433],
+            ID["EPSG",8801]],
+        PARAMETER["Longitude of natural origin",5.38763888888889,
+            ANGLEUNIT["degree",0.0174532925199433],
+            ID["EPSG",8802]],
+        PARAMETER["Scale factor at natural origin",0.9999079,
+            SCALEUNIT["unity",1],
+            ID["EPSG",8805]],
+        PARAMETER["False easting",155000,
+            LENGTHUNIT["metre",1],
+            ID["EPSG",8806]],
+        PARAMETER["False northing",463000,
+            LENGTHUNIT["metre",1],
+            ID["EPSG",8807]]],
+    CS[Cartesian,2],
+        AXIS["(E)",east,
+            ORDER[1],
+            LENGTHUNIT["metre",1,
+                ID["EPSG",9001]]],
+        AXIS["(N)",north,
+            ORDER[2],
+            LENGTHUNIT["metre",1,
+                ID["EPSG",9001]]]]
 > 
 > # compare inputs:
 > sp = as(st, "Spatial")
@@ -172,8 +258,7 @@ Coordinate Reference System:
 [8] "Attributes: < Component 5: Attributes: < Component 2: 1 string mismatch > >"                   
 > 
 > # kriging:
-> st_crs(st) = st_crs(meuse_sf) # GDAL roundtrip messes them up!
-> st_crs(meuse_sf) = st_crs(st)$proj4string # GDAL roundtrip messes them up!
+> st_crs(st) = st_crs(meuse_sf) = NA # GDAL roundtrip messes them up!
 > k_st = if (Sys.getenv("USER") == "travis") {
 + 	try(krige(log(zinc)~1, meuse_sf, st, v.fit))
 + } else {
@@ -192,9 +277,9 @@ attribute(s):
  Max.   :7.440   Max.   :0.500  
  NA's   :5009    NA's   :5009   
 dimension(s):
-  from  to offset delta                       refsys point values    
-x    1  78 178440    40 +proj=sterea +lat_0=52.15...    NA   NULL [x]
-y    1 104 333760   -40 +proj=sterea +lat_0=52.15...    NA   NULL [y]
+  from  to offset delta refsys point values    
+x    1  78 178440    40     NA    NA   NULL [x]
+y    1 104 333760   -40     NA    NA   NULL [y]
 > 
 > # handle factors, when going to stars?
 > k_sp_grd$cls = cut(k_sp_grd$var1.pred, c(0, 5, 6, 7, 8, 9))
@@ -490,4 +575,4 @@ sample    1   5     NA    NA     NA    NA sim1,...,sim5
 > 
 > proc.time()
    user  system elapsed 
-  2.430   0.067   2.488 
+  2.400   0.078   2.468 